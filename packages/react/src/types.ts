bb2dccb4114882dd23b0730587531d5786c9e4d3
Tom <214997474@qq.com>
2022-04-20 08:11:07 +0800

    chore: selector types

--- packages/react/src/types.ts
+++ packages/react/src/types.ts
@@ -2,19 +2,27 @@ import { RedoxStore, AnyModel } from '@shuvi/redox'
 
 type noExist = { [X: string | number | symbol]: never }
 
-export type ISelector<IModel extends AnyModel> = (
-	state: ReturnType<RedoxStore<IModel>['$state']>,
-	views: RedoxStore<IModel>['$views']
-) => any
+type IState<IModel extends AnyModel> = ReturnType<
+	RedoxStore<IModel>['$state']
+> &
+	noExist
+type IViews<IModel extends AnyModel> = RedoxStore<IModel>['$views'] & noExist
+type IActions<IModel extends AnyModel> = RedoxStore<IModel>['$actions']
+
+export type ISelector<IModel extends AnyModel, TReturn = any> = (
+	...args: ISelectorParams<IModel>
+) => TReturn
+
+export type ISelectorParams<IModel extends AnyModel> = [
+	IState<IModel>,
+	IViews<IModel>
+]
 
 export interface IUseModel {
-	<IModel extends AnyModel>(model: IModel): [
-		ReturnType<RedoxStore<IModel>['$state']>,
-		RedoxStore<IModel>['$actions']
-	]
+	<IModel extends AnyModel>(model: IModel): [IState<IModel>, IActions<IModel>]
 
 	<IModel extends AnyModel, Selector extends ISelector<IModel>>(
 		model: IModel,
 		selectors: Selector
-	): [ReturnType<Selector>, RedoxStore<IModel>['$actions']]
+	): [ReturnType<Selector>, IActions<IModel>]
 }
