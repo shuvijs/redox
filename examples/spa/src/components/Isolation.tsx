2f866fd50a554be89130272f1596ece275f5ce28
Tom <214997474@qq.com>
2022-05-03 21:10:29 +0800

    chore: remove no use example props

--- examples/spa/src/components/Isolation.tsx
+++ examples/spa/src/components/Isolation.tsx
@@ -4,8 +4,8 @@ import { LocalProviderA, LocalProviderB, A, B } from './useSharedModels'
 
 function Isolation() {
 	return (
-		<LocalProviderA p="A">
-			<LocalProviderB p="B">
+		<LocalProviderA>
+			<LocalProviderB>
 				<A></A>
 				<B></B>
 			</LocalProviderB>
