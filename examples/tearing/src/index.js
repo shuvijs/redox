3524ac5207ada16112ab5923b6a6c4016f12afb0
Tom <214997474@qq.com>
2022-06-06 09:37:15 +0800

    feat: redox tearing test

--- examples/tearing/src/index.js
+++ examples/tearing/src/index.js
@@ -1,7 +1,12 @@
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
 import App from './App'
+import { RedoxRoot, modelManger } from './Container'
 
 const rootElement = document.getElementById('root')
 const root = ReactDOM.createRoot(rootElement)
-root.render(<App />)
+root.render(
+	<RedoxRoot modelManager={modelManger}>
+		<App />
+	</RedoxRoot>
+)
