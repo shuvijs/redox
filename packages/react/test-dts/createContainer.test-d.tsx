311680f7ab984235b4a47ed0311e5860fc4c0a95
Andy <andy.yuanchang@gmail.com>
2022-06-20 11:24:01 +0800

    test: implement typescript test

---
+++ packages/react/test-dts/createContainer.test-d.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import { defineModel } from '@shuvi/redox'
+import { createContainer, useRootStaticModel } from '../src'
+
+import { IUseModel, IUseStaticModel } from '../src/types'
+
+import { expectType } from './'
+
+import { Action } from '../../redox/dist'
+
+function Test() {
+	const {
+		Provider: _Provider,
+		useSharedModel: _useSharedModel,
+		useStaticModel: _useStaticModel,
+	} = createContainer()
+
+	const testElement = (
+		<_Provider>
+			<></>
+		</_Provider>
+	)
+	expectType<JSX.Element>(testElement)
+	expectType<IUseModel>(_useSharedModel)
+	expectType<IUseStaticModel>(_useStaticModel)
+
+	const tempModel = defineModel({
+		state: {
+			value: {
+				n: 1,
+				s: '',
+			},
+		},
+		reducers: {
+			add(state, payload: number = 1) {
+				state.value.n += payload
+			},
+		},
+	})
+	const [state, actions] = useRootStaticModel(tempModel)
+
+	expectType<{ n: number; s: string }>(state.current.value)
+	expectType<Action<number | undefined>>(actions.add())
+}
