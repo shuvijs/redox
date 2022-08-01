fd409cc8fa164fefb4ffec59b250f68b33321fb5
liximomo <liximomo@gmail.com>
2022-08-01 14:50:37 +0800

    feat: reactive

--- packages/redox/src/views/index.ts
+++ packages/redox/src/views/index.ts
@@ -1,8 +1,8 @@
 import type { RedoxStore } from '../redoxStore'
 import { createCache } from './createCache'
 import { RedoxViews, AnyModel } from '../types'
-import validate, { isObject } from '../validate'
-import { isComplexObject } from '../utils'
+import validate from '../validate'
+import { isObject } from '../utils'
 /**
  * ICompare is tree structure, view is store view arguments and result
  *
@@ -16,35 +16,35 @@ import { isComplexObject } from '../utils'
  * 2. map value is Map<arguments, result>
  */
 interface ICompare {
-	tree: Map<
-		Record<string, any>,
-		{
-			children: Record<string, any>
-		}
-	>
-	view: Map<Function, any>
+  tree: Map<
+    Record<string, any>,
+    {
+      children: Record<string, any>
+    }
+  >
+  view: Map<Function, any>
 }
 
 const isProxy = Symbol('__isProxy')
 // const getTarget = Symbol('__target')
 
 function createProxyObjFactory() {
-	const proxyObjMap = new WeakMap<Record<string, any>, typeof Proxy>()
-	return function createProxyObj(
-		target: Record<string | symbol, any>,
-		collection: typeof getStateCollection
-	) {
-		if (target[isProxy]) {
-			return target
-		}
-		if (proxyObjMap.has(target)) {
-			return proxyObjMap.get(target)
-		}
+  const proxyObjMap = new WeakMap<Record<string, any>, typeof Proxy>()
+  return function createProxyObj(
+    target: Record<string | symbol, any>,
+    collection: typeof getStateCollection
+  ) {
+    if (target[isProxy]) {
+      return target
+    }
+    if (proxyObjMap.has(target)) {
+      return proxyObjMap.get(target)
+    }
 
-		const proxy = new Proxy(target, collection())
-		proxyObjMap.set(target, proxy)
-		return proxy
-	}
+    const proxy = new Proxy(target, collection())
+    proxyObjMap.set(target, proxy)
+    return proxy
+  }
 }
 
 /**
@@ -56,48 +56,48 @@ function createProxyObjFactory() {
  */
 // @ts-ignore
 function compareObject(prevObj: any, nextObj: any, compare: ICompare) {
-	if (!isComplexObject(prevObj)) {
-		// $state function and view fucntion comparison
-		if (typeof prevObj === 'function') {
-			// const treeMap = compare.tree.get(prevObj)
-			// $state function comparison
-			// if (treeMap) {
-			// 	const child = treeMap!.children
-			// 	const nextChild = nextObj()
-			// 	if (!isComplexObject(child)) {
-			// 		return child === nextChild
-			// 	}
-			// 	return compareObject(child, nextChild, compare)
-			// }
+  if (!isObject(prevObj)) {
+    // $state function and view fucntion comparison
+    if (typeof prevObj === 'function') {
+      // const treeMap = compare.tree.get(prevObj)
+      // $state function comparison
+      // if (treeMap) {
+      // 	const child = treeMap!.children
+      // 	const nextChild = nextObj()
+      // 	if (!isObject(child)) {
+      // 		return child === nextChild
+      // 	}
+      // 	return compareObject(child, nextChild, compare)
+      // }
 
-			// view compare, call function and compare result
-			const viewValue = compare.view.get(prevObj)
-			if (prevObj() !== viewValue) {
-				return false
-			}
-		}
-		// simple value like string number just call ===
-		return prevObj === nextObj
-	} else if (prevObj === nextObj) {
-		// Object address has not changed, children are same
-		return true
-	}
+      // view compare, call function and compare result
+      const viewValue = compare.view.get(prevObj)
+      if (prevObj() !== viewValue) {
+        return false
+      }
+    }
+    // simple value like string number just call ===
+    return prevObj === nextObj
+  } else if (prevObj === nextObj) {
+    // Object address has not changed, children are same
+    return true
+  }
 
-	const treeNode = compare.tree.get(prevObj)
-	if (!treeNode) {
-		// not visit prevObj any key, just compare with ===
-		return prevObj === nextObj
-	}
-	const child = treeNode!.children
-	const keys = Object.keys(child)
-	for (let i = 0; i < keys.length; i++) {
-		const key = keys[i]
-		const childObj = child[key]
-		if (!compareObject(childObj, nextObj[key], compare)) {
-			return false
-		}
-	}
-	return true
+  const treeNode = compare.tree.get(prevObj)
+  if (!treeNode) {
+    // not visit prevObj any key, just compare with ===
+    return prevObj === nextObj
+  }
+  const child = treeNode!.children
+  const keys = Object.keys(child)
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i]
+    const childObj = child[key]
+    if (!compareObject(childObj, nextObj[key], compare)) {
+      return false
+    }
+  }
+  return true
 }
 
 /**
@@ -107,13 +107,13 @@ function compareObject(prevObj: any, nextObj: any, compare: ICompare) {
  * @returns false => need recomputed, true => use cache value
  */
 function compareArguments(next: any, compare: ICompare) {
-	const tree = compare.tree
-	const root = Array.from(tree.keys())[0] // the Map first is tree root
-	if (!root) {
-		// use nothings
-		return true
-	}
-	return compareObject(root, next, compare)
+  const tree = compare.tree
+  const root = Array.from(tree.keys())[0] // the Map first is tree root
+  if (!root) {
+    // use nothings
+    return true
+  }
+  return compareObject(root, next, compare)
 }
 
 const stateCreateProxyObj = createProxyObjFactory()
@@ -123,222 +123,222 @@ let compareStatePos: ICompare
 let isCollectionKeys = false
 
 const getStateCollection = () => {
-	return {
-		get(target: any, p: string | symbol): any {
-			if (p === isProxy) return true
-			// if (p === getTarget) return target
-			let result = target[p]
+  return {
+    get(target: any, p: string | symbol): any {
+      if (p === isProxy) return true
+      // if (p === getTarget) return target
+      let result = target[p]
 
-			if (isCollectionKeys) {
-				const compareTree = compareStatePos.tree
-				const treeNode = compareTree.get(target)
+      if (isCollectionKeys) {
+        const compareTree = compareStatePos.tree
+        const treeNode = compareTree.get(target)
 
-				if (treeNode) {
-					treeNode.children[p as string] = result
-				} else {
-					compareTree.set(target, {
-						children: {
-							[p]: result,
-						},
-					})
-				}
+        if (treeNode) {
+          treeNode.children[p as string] = result
+        } else {
+          compareTree.set(target, {
+            children: {
+              [p]: result,
+            },
+          })
+        }
 
-				if (isComplexObject(result)) {
-					result = stateCreateProxyObj(result, getStateCollection)
-				}
-			}
-			// OwnProperty function should be a $state or view
-			if (typeof result === 'function' && target.hasOwnProperty(p)) {
-				const view = result
-				// if view result cacheView, return cache
-				if (view._isCached) {
-					return view()
-				}
-				const previousPos = compareStatePos
-				const previousCollectionStatus = isCollectionKeys
-				// call view fn
-				let res = view()
-				// if child views fn call, go on collects current scope used keys
-				if (previousCollectionStatus) {
-					isCollectionKeys = true
-					compareStatePos = previousPos
-					const compareView = compareStatePos.view
-					const viewNode = compareView.get(view)
-					if (!viewNode) {
-						compareView.set(view, res)
-					} else {
-						viewNode.set(res)
-					}
-				}
-				// cache view result
-				const cacheView = function () {
-					return res
-				}
-				cacheView._isCached = true
-				// overwrite to function for not collected view's result any more
-				target[p] = cacheView
-				return res
-			}
-			return result
-		},
-		set() {
-			if (process.env.NODE_ENV === 'development') {
-				validate(() => [[true, `not allow change any state in the view !`]])
-			}
-			return false
-		},
-	}
+        if (isObject(result)) {
+          result = stateCreateProxyObj(result, getStateCollection)
+        }
+      }
+      // OwnProperty function should be a $state or view
+      if (typeof result === 'function' && target.hasOwnProperty(p)) {
+        const view = result
+        // if view result cacheView, return cache
+        if (view._isCached) {
+          return view()
+        }
+        const previousPos = compareStatePos
+        const previousCollectionStatus = isCollectionKeys
+        // call view fn
+        let res = view()
+        // if child views fn call, go on collects current scope used keys
+        if (previousCollectionStatus) {
+          isCollectionKeys = true
+          compareStatePos = previousPos
+          const compareView = compareStatePos.view
+          const viewNode = compareView.get(view)
+          if (!viewNode) {
+            compareView.set(view, res)
+          } else {
+            viewNode.set(res)
+          }
+        }
+        // cache view result
+        const cacheView = function () {
+          return res
+        }
+        cacheView._isCached = true
+        // overwrite to function for not collected view's result any more
+        target[p] = cacheView
+        return res
+      }
+      return result
+    },
+    set() {
+      if (process.env.NODE_ENV === 'development') {
+        validate(() => [[true, `not allow change any state in the view !`]])
+      }
+      return false
+    },
+  }
 }
 
 function cacheView(
-	fn: (...args: any[]) => any
-	// _modelName: string, // just for debugging
-	// _viewsKey: any // just for debugging
+  fn: (...args: any[]) => any
+  // _modelName: string, // just for debugging
+  // _viewsKey: any // just for debugging
 ) {
-	const thisCompare: ICompare = {
-		tree: new Map(),
-		view: new Map(),
-	}
+  const thisCompare: ICompare = {
+    tree: new Map(),
+    view: new Map(),
+  }
 
-	let viewWithCache = createCache(
-		(thisPoint) => {
-			compareStatePos = thisCompare
+  let viewWithCache = createCache(
+    (thisPoint) => {
+      compareStatePos = thisCompare
 
-			// reset compare
-			resetCompare()
+      // reset compare
+      resetCompare()
 
-			const thisPointProxy = stateCreateProxyObj(thisPoint, getStateCollection)
+      const thisPointProxy = stateCreateProxyObj(thisPoint, getStateCollection)
 
-			isCollectionKeys = true // just keep collection keys when fn call
-			let res = fn.apply(thisPointProxy)
-			isCollectionKeys = false
-			// console.log(
-			// 	'modelName=>',
-			// 	_modelName,
-			// 	_viewsKey,
-			// 	thisCompare
-			// )
+      isCollectionKeys = true // just keep collection keys when fn call
+      let res = fn.apply(thisPointProxy)
+      isCollectionKeys = false
+      // console.log(
+      // 	'modelName=>',
+      // 	_modelName,
+      // 	_viewsKey,
+      // 	thisCompare
+      // )
 
-			return res
-		},
-		{
-			// false => need recomputed, true => use cache value
-			equalityCheck: (_prev: any, next: any) => {
-				// stateCompare
-				return compareArguments(next, thisCompare)
-			},
-		}
-	)
-	let originClearCache = viewWithCache.clearCache
-	viewWithCache.clearCache = function () {
-		compareStatePos = thisCompare
-		resetCompare()
-		originClearCache()
-	}
-	return viewWithCache
+      return res
+    },
+    {
+      // false => need recomputed, true => use cache value
+      equalityCheck: (_prev: any, next: any) => {
+        // stateCompare
+        return compareArguments(next, thisCompare)
+      },
+    }
+  )
+  let originClearCache = viewWithCache.clearCache
+  viewWithCache.clearCache = function () {
+    compareStatePos = thisCompare
+    resetCompare()
+    originClearCache()
+  }
+  return viewWithCache
 }
 
 function resetCompare() {
-	compareStatePos.tree.clear()
-	compareStatePos.view.clear()
+  compareStatePos.tree.clear()
+  compareStatePos.view.clear()
 }
 
 export const createViews = <IModel extends AnyModel>(
-	redoxStore: RedoxStore<IModel>
+  redoxStore: RedoxStore<IModel>
 ): void => {
-	const model = redoxStore.model
-	const views = model.views
-	if (views) {
-		if (process.env.NODE_ENV === 'development') {
-			validate(() => [
-				[
-					!isObject(views),
-					`model.views should be object, now is ${typeof views}`,
-				],
-			])
-		}
-		const proxyObj = {} as RedoxViews<IModel['views']>
-		;(Object.keys(views) as Array<keyof IModel['views']>).forEach(
-			(viewsKey) => {
-				const newView = cacheView(
-					views[viewsKey]
-					// model.name || '',
-					// viewsKey
-				)
-				// @ts-ignore
-				proxyObj[viewsKey] = function () {
-					const state = redoxStore.getState()
-					const view = redoxStore.$views
-					const selfStateAndView = Object.assign({}, state, view)
-					// generate dependsState by dependencies
-					let dependsStateAndView = {} as Record<string, any>
-					const depends = model._depends
-					if (depends) {
-						depends.forEach((depend) => {
-							const dependRedoxStore = redoxStore._cache._getRedox(depend)
-							const dependStore = Object.assign(
-								{},
-								dependRedoxStore.getState(),
-								dependRedoxStore.$views
-							)
-							dependStore['$state'] = dependRedoxStore.getState()
-							dependsStateAndView[depend.name] = dependStore
-						})
-					}
-					const thisPoint = {
-						...selfStateAndView,
-						$dep: dependsStateAndView,
-					}
-					thisPoint['$state'] = redoxStore.getState()
-					return newView(thisPoint)
-				}
-			}
-		)
-		redoxStore.$views = proxyObj
-	}
+  const model = redoxStore.model
+  const views = model.views
+  if (views) {
+    if (process.env.NODE_ENV === 'development') {
+      validate(() => [
+        [
+          !isObject(views),
+          `model.views should be object, now is ${typeof views}`,
+        ],
+      ])
+    }
+    const proxyObj = {} as RedoxViews<IModel['views']>
+    ;(Object.keys(views) as Array<keyof IModel['views']>).forEach(
+      (viewsKey) => {
+        const newView = cacheView(
+          views[viewsKey]
+          // model.name || '',
+          // viewsKey
+        )
+        // @ts-ignore
+        proxyObj[viewsKey] = function () {
+          const state = redoxStore.getState()
+          const view = redoxStore.$views
+          const selfStateAndView = Object.assign({}, state, view)
+          // generate dependsState by dependencies
+          let dependsStateAndView = {} as Record<string, any>
+          const depends = model._depends
+          if (depends) {
+            depends.forEach((depend) => {
+              const dependRedoxStore = redoxStore._cache._getRedox(depend)
+              const dependStore = Object.assign(
+                {},
+                dependRedoxStore.getState(),
+                dependRedoxStore.$views
+              )
+              dependStore['$state'] = dependRedoxStore.getState()
+              dependsStateAndView[depend.name] = dependStore
+            })
+          }
+          const thisPoint = {
+            ...selfStateAndView,
+            $dep: dependsStateAndView,
+          }
+          thisPoint['$state'] = redoxStore.getState()
+          return newView(thisPoint)
+        }
+      }
+    )
+    redoxStore.$views = proxyObj
+  }
 }
 
 export function createSelector(fn: (...args: any[]) => any) {
-	const stateAndViewsCompare: ICompare = {
-		tree: new Map(),
-		view: new Map(),
-	}
+  const stateAndViewsCompare: ICompare = {
+    tree: new Map(),
+    view: new Map(),
+  }
 
-	let selectorWithCache = createCache(
-		(stateAndViews: any) => {
-			compareStatePos = stateAndViewsCompare
+  let selectorWithCache = createCache(
+    (stateAndViews: any) => {
+      compareStatePos = stateAndViewsCompare
 
-			// reset compare
-			resetCompare()
+      // reset compare
+      resetCompare()
 
-			const stateAndViewsProxy = stateCreateProxyObj(
-				stateAndViews,
-				getStateCollection
-			)
+      const stateAndViewsProxy = stateCreateProxyObj(
+        stateAndViews,
+        getStateCollection
+      )
 
-			isCollectionKeys = true // just keep collection keys when fn call
-			let res = fn.call(null, stateAndViewsProxy)
-			isCollectionKeys = false
-			// console.log(
-			// 	'modelName=>',
-			// 	stateAndViewsCompare
-			// )
+      isCollectionKeys = true // just keep collection keys when fn call
+      let res = fn.call(null, stateAndViewsProxy)
+      isCollectionKeys = false
+      // console.log(
+      // 	'modelName=>',
+      // 	stateAndViewsCompare
+      // )
 
-			return res
-		},
-		{
-			// false => need recomputed, true => use cache value
-			equalityCheck: (_prev: any, next: any, _argsIndex: number) => {
-				// stateAndViewsCompare
-				return compareArguments(next, stateAndViewsCompare)
-			},
-		}
-	)
-	let originClearCache = selectorWithCache.clearCache
-	selectorWithCache.clearCache = function () {
-		compareStatePos = stateAndViewsCompare
-		resetCompare()
-		originClearCache()
-	}
-	return selectorWithCache
+      return res
+    },
+    {
+      // false => need recomputed, true => use cache value
+      equalityCheck: (_prev: any, next: any, _argsIndex: number) => {
+        // stateAndViewsCompare
+        return compareArguments(next, stateAndViewsCompare)
+      },
+    }
+  )
+  let originClearCache = selectorWithCache.clearCache
+  selectorWithCache.clearCache = function () {
+    compareStatePos = stateAndViewsCompare
+    resetCompare()
+    originClearCache()
+  }
+  return selectorWithCache
 }
