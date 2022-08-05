d7cb8b9c88acd4a2d76427ae380b19e371bc0270
Tom <214997474@qq.com>
2022-08-05 08:56:40 +0800

    chore: remove no use property

--- packages/redox/src/actions.ts
+++ packages/redox/src/actions.ts
@@ -2,67 +2,67 @@ import { AnyModel, Store } from './types'
 import type { RedoxStore } from './redoxStore'
 
 export const createActions = <IModel extends AnyModel>(
-	redoxStore: RedoxStore<IModel>
+  redoxStore: RedoxStore<IModel>
 ): void => {
-	const actions = redoxStore.model.actions!
-	// map actions names to dispatch actions
-	const actionKeys = Object.keys(actions) as Array<keyof IModel['actions']>
-	actionKeys.forEach((actionsName) => {
-		// @ts-ignore
-		redoxStore.$actions[actionsName as string] = function (...args: any[]) {
-			const action = actions[actionsName]
-			const storeApi = redoxStore._cache.get(redoxStore.model)
-			const dependsStoreApi = {} as any
-			const depends = redoxStore.model._depends
-			if (depends) {
-				depends.forEach((depend) => {
-					const dependApi = redoxStore._cache.get(depend)
-					const dependStore = redoxStore._cache._getRedox(depend)
-					const { $createView, $actions, $views, ...dependApiRest } = dependApi
-					const res = {
-						...(dependApiRest as Omit<
-							Store<Readonly<IModel>>,
-							'$createView' | '$actions' | '$views'
-						>),
-					}
-					Object.defineProperty(res, '$state', {
-						enumerable: true,
-						get() {
-							return dependStore.$state()
-						},
-					})
-					const views = dependStore.$views
-					Object.keys(views).forEach((viewKey) => {
-						Object.defineProperty(res, viewKey, {
-							enumerable: true,
-							get() {
-								return views[viewKey].call()
-							},
-						})
-					})
-					dependsStoreApi[depend.name] = res
-				})
-			}
-			const { $createView, $actions, $views, ...storeApiRest } = storeApi
-			const thisPoint = {
-				...storeApiRest,
-				$dep: dependsStoreApi,
-			}
-			Object.defineProperty(thisPoint, '$state', {
-				get() {
-					return redoxStore.$state()
-				},
-			})
-			const views = redoxStore.$views
-			Object.keys(views).forEach((viewKey) => {
-				Object.defineProperty(thisPoint, viewKey, {
-					enumerable: true,
-					get() {
-						return views[viewKey].call()
-					},
-				})
-			})
-			return action.call(thisPoint, ...args)
-		}
-	})
+  const actions = redoxStore.model.actions!
+  // map actions names to dispatch actions
+  const actionKeys = Object.keys(actions) as Array<keyof IModel['actions']>
+  actionKeys.forEach((actionsName) => {
+    // @ts-ignore
+    redoxStore.$actions[actionsName as string] = function (...args: any[]) {
+      const action = actions[actionsName]
+      const storeApi = redoxStore.cache.get(redoxStore.model)
+      const dependsStoreApi = {} as any
+      const depends = redoxStore.model._depends
+      if (depends) {
+        depends.forEach((depend) => {
+          const dependApi = redoxStore.cache.get(depend)
+          const dependStore = redoxStore.cache._getRedox(depend)
+          const { $createView, $actions, $views, ...dependApiRest } = dependApi
+          const res = {
+            ...(dependApiRest as Omit<
+              Store<Readonly<IModel>>,
+              '$createView' | '$actions' | '$views'
+            >),
+          }
+          Object.defineProperty(res, '$state', {
+            enumerable: true,
+            get() {
+              return dependStore.$state()
+            },
+          })
+          const views = dependStore.$views
+          Object.keys(views).forEach((viewKey) => {
+            Object.defineProperty(res, viewKey, {
+              enumerable: true,
+              get() {
+                return views[viewKey].call()
+              },
+            })
+          })
+          dependsStoreApi[depend.name] = res
+        })
+      }
+      const { $createView, $actions, $views, ...storeApiRest } = storeApi
+      const thisPoint = {
+        ...storeApiRest,
+        $dep: dependsStoreApi,
+      }
+      Object.defineProperty(thisPoint, '$state', {
+        get() {
+          return redoxStore.$state()
+        },
+      })
+      const views = redoxStore.$views
+      Object.keys(views).forEach((viewKey) => {
+        Object.defineProperty(thisPoint, viewKey, {
+          enumerable: true,
+          get() {
+            return views[viewKey].call()
+          },
+        })
+      })
+      return action.call(thisPoint, ...args)
+    }
+  })
 }
